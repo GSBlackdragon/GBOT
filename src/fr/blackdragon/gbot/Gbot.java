41e38e7b1d8ceaa553297cb516363ede2ca12ef8
CreepyDev <clemcailly@gmail.com>
2020-03-23 18:24:09 +0100

    Finalisation de la base de donnée

--- src/fr/blackdragon/gbot/Gbot.java
+++ src/fr/blackdragon/gbot/Gbot.java
@@ -15,7 +15,7 @@ public class Gbot {
 	public static String prefix = "%";
 	private static String jarFolder = Gbot.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 	private static DatabaseManager databaseManager;
-	
+
 	public static void main(String[] args) throws LoginException {
 
 		jda = new JDABuilder(AccountType.BOT).setToken(args[0]).setActivity(Activity.playing("Créer des anti-sèches"))
@@ -27,10 +27,12 @@ public class Gbot {
 			jda.awaitReady();
 		} catch (InterruptedException e) {
 		}
-		
-		databaseManager = new DatabaseManager(args[1], args[2], args[3]);
+
+		databaseManager = new DatabaseManager(args[1]
+				+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris",
+				args[2], args[3]);
 		databaseManager.open();
-		
+
 		new FormConfig();
 		Query.registerUser("Nathan", "Ferry", "23/10/2003", 2, "maths1", "pc4", "nsi1");
 	}
@@ -42,9 +44,9 @@ public class Gbot {
 	public static String getJarFolder() {
 		return jarFolder;
 	}
-	
+
 	public static DatabaseManager getDatabase() {
 		return databaseManager;
 	}
-	
+
 }
